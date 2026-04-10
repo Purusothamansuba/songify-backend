0b8e757e0fbf06eacaebb4f3d24a8aa919ff5597
Purusothamansuba <purusothaman491@gmail.com>
2026-04-10 12:43:58 +0530

    force migration debug

--- songs/serializers.py
+++ songs/serializers.py
@@ -1,36 +1,31 @@
 from rest_framework import serializers
 from .models import Song, Playlist, PlaylistSong, Like, History
 
-
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
 
-
 class SongListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'artist', 'file_url', 'cover_image']
-
+        # FIX: Added file_url, duration, and cover_image so the player has what it needs
+        fields = ['id', 'title', 'artist', 'file_url', 'duration', 'cover_image']
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
         fields = '__all__'
 
-
 class PlaylistSongSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlaylistSong
         fields = '__all__'
 
-
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = '__all__'
-
 
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
